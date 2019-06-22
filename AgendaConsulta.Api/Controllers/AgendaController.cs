fadec5806b5d9f2194176d80a0d90073b867ca62
Gasma <gabriel.moreira.alvarenga@gmail.com>
2019-06-22 16:48:18 -0300

    Adiciona Agenda Controller

---
+++ AgendaConsulta.Api/Controllers/AgendaController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AgendaConsulta.Api.Models;
+
+namespace AgendaConsulta.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgendaController : ControllerBase
+    {
+        private readonly AgendaContext _context;
+
+        public AgendaController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Agenda
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Agenda>>> GetAgenda()
+        {
+            return await _context.Agenda.ToListAsync();
+        }
+
+        // GET: api/Agenda/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Agenda>> GetAgenda(int id)
+        {
+            var agenda = await _context.Agenda.FindAsync(id);
+
+            if (agenda == null)
+            {
+                return NotFound();
+            }
+
+            return agenda;
+        }
+
+        // PUT: api/Agenda/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAgenda(int id, Agenda agenda)
+        {
+            if (id != agenda.AgendaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(agenda).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AgendaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Agenda
+        [HttpPost]
+        public async Task<ActionResult<Agenda>> PostAgenda(Agenda agenda)
+        {
+            _context.Agenda.Add(agenda);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAgenda", new { id = agenda.AgendaId }, agenda);
+        }
+
+        // DELETE: api/Agenda/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Agenda>> DeleteAgenda(int id)
+        {
+            var agenda = await _context.Agenda.FindAsync(id);
+            if (agenda == null)
+            {
+                return NotFound();
+            }
+
+            _context.Agenda.Remove(agenda);
+            await _context.SaveChangesAsync();
+
+            return agenda;
+        }
+
+        private bool AgendaExists(int id)
+        {
+            return _context.Agenda.Any(e => e.AgendaId == id);
+        }
+    }
+}
