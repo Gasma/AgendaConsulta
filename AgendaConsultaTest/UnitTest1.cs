30b1bd34d6793d31e39ffe93c9ebec3bbc0191a4
Gasma <gabriel.moreira.alvarenga@gmail.com>
2019-06-24 02:11:15 -0300

    Adiciona modulo de teste

---
+++ AgendaConsultaTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using AgendaConsulta.Api.Controllers;
+using AgendaConsulta.Api.Models;
+using AgendaConsulta.Api.Services;
+using System;
+using Xunit;
+
+namespace AgendaConsultaTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void SalvarAgendaTest()
+        {
+            //var agendaCon = new AgendaController();
+
+        }
+    }
+}
